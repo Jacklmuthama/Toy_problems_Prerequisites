fbcd38d286940846625ff356857db98ac0e90020
Jackline <jackline.muthama@student.moringaschool.com>
2022-11-25 15:49:50 +0300

    completed solution1

--- scr/solution1.js
+++ scr/solution1.js
@@ -1,1 +1,21 @@
-
+function studentGrade(marks) {
+    if (marks>=79) {
+        return "A";
+    }
+    if (marks<=78 && marks>=60) {
+        return "B";
+    }
+    if (marks<=59 && marks>=49) {
+        return "C";
+    }
+    else if (marks<=48 && marks>=40) {
+        return "D";
+    }
+    else{
+        return "E";
+    }
+}
+function handleClick(event) {
+    let marks= document.getElementById("marks").value
+    document.getElementById("answer").textContent=studentGrade(marks)
+}
--- scr/solution2.js
+++ scr/solution2.js
@@ -1,1 +1,8 @@
-
+function carSpeed(input) {
+    if (input<=70) {
+        return "Ok";
+    }
+    if (input>70===((n-70)/5)) {
+        return "License suspended”;
+    }
+}
