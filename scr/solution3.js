1305e514b8f6f35b94d6725f522c23d0b122c7f8
Jackline <jackline.muthama@student.moringaschool.com>
2022-11-28 18:58:51 +0300

    changed switch to if)

--- scr/solution3.js
+++ scr/solution3.js
@@ -14,58 +14,57 @@ function taxableIncome(gross,...deductions) {
 }
 function deductions(gross) {
     let NHIF=0 , NSSF=200;
-    switch(gross) {
-        case gross <= 5999:
-            NHIF =150 
-            break
-        case gross >= 6000 && gross <= 7999:
+        if (gross <= 5999) {
+        NHIF =150           
+        }
+        else if( gross >= 6000 && gross <= 7999) {
             NHIF =300
-            break
-        case gross >= 8000 && gross <= 11999:
+        }
+        else if( gross >= 8000 && gross <= 11999) {            
             NHIF =400
-            break
-        case gross >= 12000 && gross <= 14999:
+        }
+        else if( gross >= 12000 && gross <= 14999) {
             NHIF =500
-            break
-        case gross >= 15000 && gross <= 19999:
+        }
+        else if( gross >= 15000 && gross <= 19999) {
             NHIF =600
-            break
-        case gross >= 20000 && gross <= 24999:
+        }
+        else if( gross >= 20000 && gross <= 24999) {
             NHIF =750
-            break
-        case gross >= 25000 && gross <= 29999:
+        }
+        else if( gross >= 25000 && gross <= 29999) {
             NHIF =850
-            break
-        case gross >= 30000 && gross <= 34999:
+        }
+        else if( gross >= 30000 && gross <= 34999) {
             NHIF =900
-            break
-        case gross >= 35000 && gross <= 39999:
+        }
+        else if( gross >= 35000 && gross <= 39999) {
             NHIF =950
-            break
-        case gross >= 40000 && gross <= 44999:
+        }
+        else if( gross >= 40000 && gross <= 44999) {
             NHIF =1000
-            break
-        case gross >= 45000 && gross <= 49999:
+        }
+        else if( gross >= 45000 && gross <= 49999) {
             NHIF =1100
-            break
-        case gross >= 50000 && gross <= 59999:
+        }
+        else if( gross >= 50000 && gross <= 59999) {
             NHIF =1200
-            break
-        case gross >= 60000 && gross <= 69999:
+        }
+        else if( gross >= 60000 && gross <= 69999) {
             NHIF =1300
-            break
-        case gross >= 70000 && gross <= 79999:
+        }
+        else if( gross >= 70000 && gross <= 79999) {
             NHIF =1400
-            break
-        case gross >= 80000 && gross <= 89999:
+        }
+        else if( gross >= 80000 && gross <= 89999) {
             NHIF =1500
-            break
-        case gross >= 90000 && gross <= 99999:
+        }
+        else if( gross >= 90000 && gross <= 99999) {
             NHIF =1600
-            break
-        case gross >= 100000 :
-            NHIF =1700 
-    }
+        }
+        else if(gross >= 100000) {
+            NHIF= 1700}
+    
     return [NHIF, NSSF]
 }
 function payAsYouEarn(taxable){
